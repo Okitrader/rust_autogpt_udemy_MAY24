eb1487a1587736871d18da09fc4f01fdcb9ed1b7
okitrader <lites_eugene@hotmail.com>
2024-05-01 06:48:40 +0900

    upto 19
    updated readme

---
+++ my_library/target/debug/deps/my_library-658dbd9beae51158.d
@@ -0,0 +1,7 @@
+/Users/okitrader/Library/Mobile Documents/com~apple~CloudDocs/rust_autogpt_udemy_MAY24/my_library/target/debug/deps/libmy_library-658dbd9beae51158.rmeta: src/lib.rs
+
+/Users/okitrader/Library/Mobile Documents/com~apple~CloudDocs/rust_autogpt_udemy_MAY24/my_library/target/debug/deps/libmy_library-658dbd9beae51158.rlib: src/lib.rs
+
+/Users/okitrader/Library/Mobile Documents/com~apple~CloudDocs/rust_autogpt_udemy_MAY24/my_library/target/debug/deps/my_library-658dbd9beae51158.d: src/lib.rs
+
+src/lib.rs:
---
+++ my_library/target/debug/deps/my_library-8e9c5707c6ee0cd9.d
@@ -0,0 +1,5 @@
+/Users/okitrader/Library/Mobile Documents/com~apple~CloudDocs/rust_autogpt_udemy_MAY24/my_library/target/debug/deps/my_library-8e9c5707c6ee0cd9: src/lib.rs
+
+/Users/okitrader/Library/Mobile Documents/com~apple~CloudDocs/rust_autogpt_udemy_MAY24/my_library/target/debug/deps/my_library-8e9c5707c6ee0cd9.d: src/lib.rs
+
+src/lib.rs:
